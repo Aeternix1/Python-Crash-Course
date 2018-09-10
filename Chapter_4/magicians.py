cd417421934d2b51a41719a8ad2d5f775a84f9e0
varghese.kallukalam@gmail.com <varghese.kallukalam@gmail.com>
2018-09-10 17:45:14 +1000

    added Chapter 4

---
+++ Chapter_4/magicians.py
@@ -0,0 +1,14 @@
+#Using a for loop to navigate through the list
+magicians = ['alice', 'david', 'carolina']
+#The value is stored in the variable magician, choose reasonable variable names
+#Once we reach the end of the list no more values are stored and the for loop is
+#Exite
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you everyone. That was a great magic show!")
+
+
+
+
---
+++ Chapter_4/pizza.py
@@ -0,0 +1,7 @@
+pizzas = ['Margherita', 'Pineapple', 'Vegan']
+
+for pizza in pizzas:
+    print(pizza.title())
+    print("I really like " + pizza + " pizza!")
+
+print("I really like pizza!")
