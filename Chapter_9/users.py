99734f73d2e8aae20d4dab48b53c361d6f7520c6
varghese.kallukalam@gmail.com <varghese.kallukalam@gmail.com>
2018-09-27 10:28:47 +1000

    added increment feature to user class

--- Chapter_9/users.py
+++ Chapter_9/users.py
@@ -9,6 +9,7 @@ class User():
         self.fullname = first_name + " " + last_name
         self.age = age
         self.dob = dob
+        self.login_attempts = 0
 
     def describe_user(self):
         print("First name: " + self.first_name.title())
@@ -19,8 +20,29 @@ class User():
     def greet_user(self):
         print("Hello there " + self.fullname)
 
+    def view_login_attempts(self):
+        """View the login attempts of the user"""
+        print("The user has " + str(self.login_attempts) + "login attempts")
+
+    def increment_login_attempts(self):
+        """Increments the login attempts by 1"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset login attempts"""
+        self.login_attempts = 0 
+
 ajay = User('ajay', 'kallukalam', 22, '23rd of September')
 
 ajay.describe_user()
 ajay.greet_user()
+
+ajay.view_login_attempts()
+ajay.increment_login_attempts()
+ajay.increment_login_attempts()
+ajay.view_login_attempts()
+
+ajay.reset_login_attempts()
+ajay.view_login_attempts()
+
 
