d845c7688ed65eabff4e5764b61a55000212bd3b
varghese.kallukalam@gmail.com <varghese.kallukalam@gmail.com>
2018-09-29 10:07:21 +1000

    using the replace method in ex 10.2

--- Chapter_10/learning_python.py
+++ Chapter_10/learning_python.py
@@ -1,18 +1,20 @@
 filename = 'learning_python.txt'
 
-#Reading the entire file once
-with open(filename) as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+# #Reading the entire file once
+# with open(filename) as file_object:
+    # contents = file_object.read()
+    # print(contents.rstrip())
 
 #Looping over the file object
 with open(filename) as file_object:
     for line in file_object:
+        line = line.replace('Python', 'C')
         print(line.rstrip())
 
-#Store in list and looping thorugh
-with open(filename) as file_object:
-    lines = file_object.readlines()
-    for line in lines:
-        print(line.rstrip())
+
+# #Store in list and looping thorugh
+# with open(filename) as file_object:
+    # lines = file_object.readlines()
+    # for line in lines:
+        # print(line.rstrip())
 
