71a6f2d692e867d2146cfb6bcf52bfca18c4af5d
Ajay <varghese.kallukalam@zoho.com>
2019-01-03 09:27:54 +1100

    Multiple walks generated

--- Data_Visualisation/rw_visual.py
+++ Data_Visualisation/rw_visual.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-#Make a random walk and plot the points 
-rw = RandomWalk()
-rw.fill_walk()
+#Generate multiple random walks
+while True:
+    #Make a random walk and plot the points 
+    rw = RandomWalk()
+    rw.fill_walk()
+    
+    plt.scatter(rw.x_values, rw.y_values, s=5)
+    plt.show()
 
-plt.scatter(rw.x_values, rw.y_values, s=5)
+    keep_running = input("Make another walk? (y/n)")
+    if keep_running == 'n':
+        break
 
-plt.show()
