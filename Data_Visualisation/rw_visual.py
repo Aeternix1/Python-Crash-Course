048dea7fac54303746f2dc5cc77b52e8a30fb864
Ajay <varghese.kallukalam@zoho.com>
2019-01-03 09:34:45 +1100

    removed the axis

--- Data_Visualisation/rw_visual.py
+++ Data_Visualisation/rw_visual.py
@@ -6,11 +6,20 @@ while True:
     #Make a random walk and plot the points 
     rw = RandomWalk()
     rw.fill_walk()
-    
-    plt.scatter(rw.x_values, rw.y_values, s=5)
+   
+    #Coloring the points
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,\
+            edgecolors='none', s=5)
+
+    #Emphasise the first and the last points
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',\
+            s=20)
+    plt.scatter(rw.x_values[0], rw.y_values[0], c='red', edgecolors='none',\
+            s=20)
     plt.show()
 
-    keep_running = input("Make another walk? (y/n)")
+    keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
         break
 
