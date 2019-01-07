e64672a67429a8109d8cb923145a7d9eaca3d526
Ajay <varghese.kallukalam@zoho.com>
2019-01-07 13:59:17 +1100

    abandoned data-analytics

---
+++ Data_Visualisation/high_low_san_fran.py
@@ -0,0 +1,8 @@
+#Plotting the temperature lows and highs for San Fransisco
+
+from matplotlib import pyplot as plt
+from datetime import datetime 
+import csv
+
+filename=#abandoning this project for the minute not working so well
+
